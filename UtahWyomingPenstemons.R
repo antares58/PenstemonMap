d0be3cc09cb411eb2425db14098f7ea332384c11
antares58 <elgenlee@pm.me>
2022-06-16 22:15:16 -0700

    Experimental functions to expand vector data and work with data structures.

--- UtahWyomingPenstemons.R
+++ UtahWyomingPenstemons.R
@@ -5,7 +5,7 @@ library(stringr)
 #PENSTEMON_JSON = "UtahWyomingPenstemons.JSON"
 PENSTEMON_DB_FILENAME = "Penstemons.txt"
 PENSTEMON_NAMES_FILENAME = "PenstemonAlternateNames.txt"
-COUNTY_FIPS_FILENAME = "CountyFIPSCodes.txt"
+COUNTY_FIPS_FILENAME = "UTWY_CountyFIPSCodes.txt" #"CountyFIPSCodes.txt"
 STATE_FIPS_FILENAME = "StateFIPSCodes.txt"
 
 DB_PATH = paste0(getwd(), "/", PENSTEMON_DB_FILENAME)
@@ -15,14 +15,17 @@ STATE_FIPS_PATH = paste0(getwd(), "/", STATE_FIPS_FILENAME)
 
 PLANT_DB <-NA
     PLANT_STATES <- data.table(PlantID = character(), StateCode = character())
-    PLANT_COUNTIES <- data.table(PlantID = character(), CountyCode = character())
+    PLANT_COUNTIES <- data.table(PlantID = character(), FIPS = integer())
     BLOOM_COLORS <- data.table(PlantID = character(), BloomColor = character())
     BLOOM_MONTHS <- data.table(PlantID = character(), BloomMonth = integer())
 PLANT_NAMES <- data.table(Name = character(), 
                           Language = character(),
                           PlantID = character())
-COUNTY_FIPS_DATA <- NA
-STATE_FIPS_DATA <- NA
+COUNTY_DATA <- NA
+COUNTY_DEFAULT <- 99999
+
+WORKING_DATA <- NA # The set of PlantIDs that match the selection criteria
+WORKING_FIPS <- NA # The set of county FIPS codes that match selection criteria
 
 # Control flow for reading data from disk
 ERR <- FALSE
@@ -37,7 +40,6 @@ tryCatch(
         message(paste0("Penstemon data file \'", DB_PATH, "\' not found."))
     }
 )
-# Make sure the penstemon names configuration file is accessible
 
 # Make sure the alternate names configuration file is accessible
 tryCatch(
@@ -72,15 +74,34 @@ tryCatch(
 
 # Read in state and county FIPS data
 if(!ERR) {
-    if(is.na(COUNTY_FIPS_DATA)) COUNTY_FIPS_DATA <<- fread(COUNTY_FIPS_PATH)
-    if(is.na(STATE_FIPS_DATA)) STATE_FIPS_DATA <<- fread(STATE_FIPS_PATH)
-    if( (is.null(COUNTY_FIPS_DATA) || is.na(COUNTY_FIPS_DATA) || 
-        is.null(STATE_FIPS_DATA) || is.na(STATE_FIPS_DATA)) ) ERR <<- TRUE
+    if(is.na(COUNTY_DATA)) COUNTY_DATA <<- fread(COUNTY_FIPS_PATH)
+    if( (is.null(COUNTY_DATA) || is.na(COUNTY_DATA)) ) ERR <<- TRUE
+         
+    if(!ERR) {
+        state_fips <- fread(STATE_FIPS_PATH)
+        if( (is.null(state_fips) || is.na(state_fips)) ) ERR <<- TRUE
+        else {
+            COUNTY_DATA[, StateName := state_fips[STUSAB == .BY, STATE_NAME], by = list(State)]
+            COUNTY_DATA[, StateFIPS := state_fips[STUSAB == .BY, STATE], by = list(State)]
+            COUNTY_DATA[, InputName := paste(State, ":", Name)]
+            rm(state_fips)
+        }
+    }
 }
 
 # Read in the plant database file; expand, and cache certain list columns.
 if((!ERR & is.na(PLANT_DB))) {
-    PLANT_DB <<- fread(DB_PATH,
+#    PLANT_DB <<- fread(DB_PATH,
+#                           key = "PlantID")
+    # Grab the column header
+    header <- fread(DB_PATH, nrows = 1)
+    
+    # Retrieve only species found in Utah or Wyoming
+    command <- sprintf(
+        "grep -e 'UT' -e 'WY' %s", DB_PATH)
+    
+    PLANT_DB <<- fread(cmd = command,
+                           col.names = names(header),
                            key = "PlantID")
     
     # Extract state distribution data into a tidy data table by PlantID
@@ -94,13 +115,16 @@ if((!ERR & is.na(PLANT_DB))) {
     # Extract county distribution data into a tidy data table by PlantID
     extractCounties <- function(id, countiesString) {
         if(any(nzchar(countiesString)))
-            counties <- trimws(unlist(str_split(countiesString, ",")))
+            counties <- as.integer(unlist(str_split(countiesString, ",")))
         else
-            counties = "all"
+            counties = COUNTY_DEFAULT
         PLANT_COUNTIES <<- rbind(PLANT_COUNTIES, 
-                               data.table(PlantID = id, CountyCode = counties))
+                               data.table(PlantID = id, FIPS = counties))
     }
     mapply(extractCounties, PLANT_DB$PlantID, PLANT_DB$CountyCodes)
+    
+    # Restrict FIPS data to states/territories that have penstemon species.
+    COUNTY_DATA <<- COUNTY_DATA[State %chin% unique(PLANT_STATES$StateCode)]
     
     # Extract bloom colors data into a tidy data table by PlantID
     extractColors <- function(id, colorsString) {
@@ -145,6 +169,28 @@ if((!ERR & nrow(PLANT_NAMES) == 0)) {
     }
 }
 
+resetWorkingData <- function() {
+    states <- sort(unique(PLANT_STATES$StateCode))
+    workingData <- PLANT_DB[StateCode == states[1]]
+}
 
+languages <- function() {
+    unique(PLANT_NAMES$Language)
+}
 
+states <- function() {
+    return(c("Utah", "Wyoming"))
+}
 
+countiesForStates <- function(selectedStates) {
+    if((is.na(selectedStates) || is.null(selectedStates))) {
+        return(c("<All>"))
+    } else {
+        WORKING_FIPS_DATA <<- COUNTY_DATA[State %chin% selectedStates, .(FIPS, State)]
+        return(COUNTY_DATA[State %chin% selectedStates, InputName])
+    }
+}
+
+plantNames <- function() {
+    WORKING_DB <<- PLANT_COUNTIES[CountyCode %in% WORKING_FIPS_DATA]
+}
